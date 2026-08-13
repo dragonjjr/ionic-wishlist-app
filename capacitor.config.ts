22907c734426a92713423e39ec350e3ac4862fd4
longmv <longmv@aureole-it.vn>
2026-08-13 18:03:28 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'team-wishlist',
+  webDir: 'www'
+};
+
+export default config;
